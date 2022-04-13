8adcde56028ffad503ebfe53cacc9d1b7d01738e
Shukur Sabzaliev <shakhansho.sabzaliev_2023@ucentralasia.org>
2022-04-13 10:29:08 +0600

    List of Environmental Variabls
    API keys

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+TOKEN = '5154491554:AAFqqw41D9Sxa_WWu2-KA1LrYpyooy59weE'
+api_secret = 'ByUw3qjQQnqMNFC9Qpdpc-0uWLw'
+api_key = '247757221379347'
+FAUNA_KEY = 'fnAEiCj2nRACS1r5npPyu1EBR07SGCD9vwFOedXJ'
+
+
